5cf7869f16168d063236e51d8190194877edcbac
Mallory Shackford <93612680+MalloryShackford@users.noreply.github.com>
2023-03-16 09:28:39 -0400

    first commit

---
+++ client/src/components/Nav.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import whiteLogo from '../images/tinder_logo_white.png'
+import colorLogo from '../images/color-logo-tinder.png'
+
+const Nav = ({minimal, setShowModal, showModal, setIsSignUp}) => {
+
+  const handleClick = () => {
+    setShowModal(true)
+    setIsSignUp(false)
+  }
+
+  const authToken = false
+  return (
+    <nav>
+      <div className="logo-container">
+        <img className="logo" src={minimal ? colorLogo : whiteLogo} alt="tinderLogo"/>
+      </div>
+      {!authToken && !minimal && <button 
+        className="nav-button"
+        onClick={handleClick}
+        disabled={showModal}
+        >Log in</button>}
+
+    </nav>
+  )
+}
+
+export default Nav
